3823f281a71e7e68ec99e76f2c6abcb9aafd76c9
Jacques-Pienaar <42588240+Jacques-Pienaar@users.noreply.github.com>
2019-05-09 12:10:54 +0200

    Add files via upload

--- Datahandler.cs
+++ Datahandler.cs
@@ -1,29 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
-namespace MilitaryCommunications
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace TestingForm_MilitaryComms
 {
-    abstract public class Datahandler
+    abstract class Datahandler
     {
+
+        SqlConnectionStringBuilder connection = new SqlConnectionStringBuilder();
+        SqlConnection conn = new SqlConnection();
+        private string ConnectionString = @"Data Source=DESKTOP-RLPPP3A;Initial Catalog=MilitaryComms;Integrated Security=True";
+
         public Datahandler()
         {
+            connection.DataSource = @"DESKTOP-RLPPP3A";
+            connection.InitialCatalog = "MilitaryComms";
+            connection.IntegratedSecurity = true;
+            conn = new SqlConnection(connection.ConnectionString);
         }
+
+    
+
 
         /* 
          * Gets all the officers from the database and returns it in a list 
          * so that the list can then be compared in other functions. It can be used to login
          * a user or display all officers
-         */       
+         */
+
+ 
+
         public List<Officer> GetOfficers()
         {
             List<Officer> listToReturn = new List<Officer>();
 
-            /* Return dummy values till database is connected */
-            listToReturn.Add(new Officer("Herco", "Bezuidenhout", 20, 0, "hercobez@gmail.com", "herco1999"));
-            listToReturn.Add(new Officer("Jandre", "Bezuidenhout", 15, 1, "jandrebez@gmail.com", "jandre2005"));
-            listToReturn.Add(new Officer("Billy", "Jones", 34, 0, "billyjones@gmail.com", "billyisgay"));
-            listToReturn.Add(new Officer("Andrew", "Holt", 32, 2, "andrewholt@gmail.com", "andrewisgreat"));
+
 
             return listToReturn;
         }
+
+         
     }
 }
